ccb5954837f835fcc5a50fe08acc6473c6381bf1
Aryan Singh <aryansingh143001@gmail.com>
2025-04-24 20:36:44 +0530

    files updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          'import',
+          'mixed-decls',
+          'color-functions',
+          'global-builtin',
+          'legacy-js-api',
+        ],
+      },
+    },
+  },
 })
